bfd0deb984610917d2c5348f7e90907e78bc7a2b
PNRxA <pnrunham@gmail.com>
2017-07-09 13:41:46 +1000

    V0.5 infinite generation with caves/trees and some weird terrain clipping going on

---
+++ Assets/Scripts/Serialization.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+using System;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Runtime.Serialization;
+
+public static class Serialization
+{
+    public static string saveFolderName = "voxelGameSaves";
+    public static string SaveLocation(string worldName)
+    {
+        string SaveLocation = saveFolderName + "/" + worldName + "/";
+
+        if (!Directory.Exists(SaveLocation))
+        {
+            Directory.CreateDirectory(SaveLocation);
+        }
+
+        return SaveLocation;
+    }
+
+    public static string FileName(WorldPos chunkLocation)
+    {
+        string FileName = chunkLocation.x + "," + chunkLocation.y + "," + chunkLocation.z + ".bin";
+
+        return FileName;
+    }
+
+    public static void SaveChunk(Chunk chunk)
+    {
+        Save save = new Save(chunk);
+        if (save.blocks.Count == 0)
+        {
+            return;
+        }
+
+        string saveFile = SaveLocation(chunk.world.worldName);
+        saveFile += FileName(chunk.pos);
+
+        IFormatter formatter = new BinaryFormatter();
+        Stream stream = new FileStream(saveFile, FileMode.Create, FileAccess.Write, FileShare.None);
+        formatter.Serialize(stream, save);
+        stream.Close();
+    }
+
+    public static bool Load(Chunk chunk)
+    {
+        string saveFile = SaveLocation(chunk.world.worldName);
+        saveFile += FileName(chunk.pos);
+
+        if (!File.Exists(saveFile))
+        {
+            return false;
+        }
+
+        IFormatter formatter = new BinaryFormatter();
+        FileStream stream = new FileStream(saveFile, FileMode.Open);
+
+        Save save = (Save)formatter.Deserialize(stream);
+        foreach (var blocks in save.blocks)
+        {
+            chunk.blocks[blocks.Key.x, blocks.Key.y, blocks.Key.z] = blocks.Value;
+        }
+        stream.Close();
+        return true;
+    }
+}
